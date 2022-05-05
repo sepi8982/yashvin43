908997886a7800e01c308884ac27b0dc848373b8
sepi8982 <pathaksapna27@gmail.com>
2022-05-05 01:54:00 -0700

    adding prime1 file

---
+++ prime1.py
@@ -0,0 +1,75 @@
+'''
+num=1
+
+while num<10:
+    if num>1:
+        i=2
+        while i <= num // 2:
+            if num%i ==0:
+                break
+            i+=1
+        else:
+            print(num)
+    num+=1
+'''
+##########################################
+'''
+a=0
+b=1
+count = 0
+
+while count <= 10:
+    print(a)
+    c = a  + b
+    a,b = b,c
+    count+=1
+
+'''
+
+#############################################
+'''
+string ="hello welcome"
+i=0
+while i<len(string):
+    print(i,string[i])
+    i+=1
+
+'''
+
+###############################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
